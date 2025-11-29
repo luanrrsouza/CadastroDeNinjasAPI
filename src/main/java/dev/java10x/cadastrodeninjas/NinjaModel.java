a90faf702c22f01a306ce83d59b2913cc68d1254
luanrrsouza <luanrrdsouza@gmail.com>
2025-11-29 20:44:21 -0300

    feat: NinjaModel.

---
+++ src/main/java/dev/java10x/cadastrodeninjas/NinjaModel.java
@@ -0,0 +1,58 @@
+package dev.java10x.cadastrodeninjas;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro_de_ninjas")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
