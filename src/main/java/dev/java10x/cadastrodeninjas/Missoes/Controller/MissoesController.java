0740cf69dc0cdd3884a470297927b9f978798485
luanrrsouza <luanrrdsouza@gmail.com>
2025-12-01 10:24:32 -0300

    feat: Cria as rotas do controller de missão.

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/Controller/MissoesController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/Controller/MissoesController.java
@@ -1,4 +1,28 @@
 package dev.java10x.cadastrodeninjas.Missoes.Controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missão alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missão deletada";
+    }
+
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Missoes Listadas";
+    }
 }
